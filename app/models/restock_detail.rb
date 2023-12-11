0574e96df648a565606783fea2d8ed41dd95708b
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-12-11 11:44:35 +0630

    Add one to many associations between stock and restock_detail model

--- app/models/restock_detail.rb
+++ app/models/restock_detail.rb
@@ -2,6 +2,7 @@
 
 class RestockDetail < ApplicationRecord
   belongs_to :restock
+  belongs_to :stock
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :expiry_date, presence: true
