8e6b6bce34de529730db9ef34f8b837c1a904385
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-12-11 11:25:48 +0630

    Add set_available_default method in truck

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -4,7 +4,14 @@ class Truck < ApplicationRecord
   has_many :deliveries
 
   validates :license, presence: true
-  validates :driver, presence: true, length: { maximum: 15 }, type: :string
+  validates :driver, presence: true, length: { maximum: 15 }
   validates :capacity, presence: true
   validates :available, inclusion: { in: [true, false] }
+
+  after_initialize :set_available_default
+  private 
+
+  def set_available_default
+    self.available ||= false
+  end
 end
