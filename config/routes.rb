212d263239859c49be6e1b0e8dc96de6156ce9a0
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-12-11 14:29:00 +0630

    Add routes and remove region validation rule for delivery

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :preorders
+    end
+  end
+  
   devise_for :users, path: '', path_names: {
                                  sign_in: 'login',
                                  sign_out: 'logout',
