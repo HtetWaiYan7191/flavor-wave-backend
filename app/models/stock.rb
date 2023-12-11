9933f4aa4e106dcd517580ec585573e69c84bd80
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-12-11 12:11:50 +0630

    Add has_many relation ship to preorder_items

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Stock < ApplicationRecord
+  has_many :preorder_items
   has_many :preorders, through: :preorder_items
   has_many :stock_details
   has_many :restock_details
