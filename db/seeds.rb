8f9623c3781d27553f40648b77efc1343c87654d
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-12-11 12:12:10 +0630

    Add common data created code to db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+client = Client.create(name: 'kfc', region: 'yangon', phone: '01231414', address: 'No(40) street yangon' )
+stock_1 = Stock.create(name: 'chicken', quantity: 3, unit_price: 2000)
+
+preorder_1 = Preorder.create(client_id: client.id, quantity: 20, order_date: '12/2/2020', order_status: 'pending', total: 20000, user_id: User.first.id)
