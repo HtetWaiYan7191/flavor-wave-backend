ac91378623adebdca2c182c40e49b25925964559
Thiri <thirihtet246@gmail.com>
2023-12-14 23:06:56 +0630

    Update delivery_date default value

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -5,4 +5,11 @@ class Delivery < ApplicationRecord
   belongs_to :truck
 
   validates :delivery_date, presence: true, allow_blank: false
+  after_initialize :set_default_date
+
+  private
+
+  def set_default_date
+    update(delivery_date: (Time.zone.now + 3.days).strftime('%d/%m/%Y'))
+  end
 end
