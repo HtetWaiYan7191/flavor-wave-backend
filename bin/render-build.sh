a8400f3541c1ca3208269e0eeca8e0ed614821a3
Thiri <thirihtet246@gmail.com>
2023-12-15 01:04:41 +0630

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,5 @@ set -o errexit
 bundle install
 # bundle exec rake assets:precompile
 # bundle exec rake assets:clean
-bundle exec rails db:reset
 bundle exec rails db:migrate
 bundle exec rails db:seed
